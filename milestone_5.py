74f04db906b6cb02c84d62ced7e86c39089077c5
aaboyle878 <65974028+aaboyle878@users.noreply.github.com>
2023-11-04 12:39:26 +0000

    refactored to include useful feed back to user

--- milestone_5.py
+++ milestone_5.py
@@ -1,9 +1,7 @@
 import random
 
 def play_game(word_list):
-    num_lives = 5
-    word_list = word_list
-    game = Hangman(word_list, num_lives)
+    game = Hangman(word_list, num_lives = 5)
     while True:
         if game.num_lives == 0:
             return print("You lost")
@@ -21,10 +19,12 @@ class Hangman():
         self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(list(self.word)))
+        print(f"The Mystery word has {self.num_letters} characters")
+        
 
     def ask_for_input(self):
-        print(self.word_guessed)
         while True:
+            print(self.word_guessed)
             guess = input('Please enter a single letter: ')
             if len(guess) != 1 or guess.isalpha() != True:
                 print("Invalid letter. Please, enter a single alphabetical character.")
