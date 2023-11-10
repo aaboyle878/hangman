d77dae348404dc4836bd2da07805980597384f23
aaboyle878 <65974028+aaboyle878@users.noreply.github.com>
2023-11-10 12:43:56 +0000

    refactors milestone_5.py to remove pass on line 12, include __name__ condition and remove letter guess duplication based on case

--- milestone_5.py
+++ milestone_5.py
@@ -1,5 +1,6 @@
 import random
 
+
 def play_game(word_list):
     game = Hangman(word_list, num_lives = 5)
     while True:
@@ -9,7 +10,6 @@ def play_game(word_list):
             game.ask_for_input()
         else:
             return print("Congratulation, You have won!")
-    pass
 
 class Hangman():
     def __init__(self, word_list, num_lives = 5):
@@ -26,6 +26,7 @@ class Hangman():
         while True:
             print(self.word_guessed)
             guess = input('Please enter a single letter: ')
+            guess = guess.lower()
             if len(guess) != 1 or guess.isalpha() != True:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
@@ -36,7 +37,6 @@ class Hangman():
         self.check_guess(guess)
     
     def check_guess(self,guess):
-        guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             counter = 0
@@ -52,5 +52,6 @@ class Hangman():
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left")
-
-play_game(['apple', 'orange', 'grape', 'peach', 'blueberry'])
+            
+if __name__ == "__main__":
+    play_game(['apple', 'orange', 'grape', 'peach', 'blueberry'])
